9054d2048a8140dc663c6e86a0398c891c696b29
Jonathan Cousins <evolutionxbox@gmail.com>
2018-03-09 17:48:04 +0000

    Add state to the App component (=> the username) and pass the username to the UserOutput component - Closes #5

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import UserInput from './UserInput';
 import UserOutput from './UserOutput';
 
 class App extends Component {
+  state = {
+    username: "evolutionxbox"
+  }
+
   render() {
     return (
       <div className="App">
-        <UserOutput username="evolutionxbox" />
+        <UserOutput username={this.state.username} />
         <UserOutput />
         <UserOutput />
       </div>
