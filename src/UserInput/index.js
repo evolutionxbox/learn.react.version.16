56f5eed3723e787d7f6b627ef46d9db81e8d8ba6
Jonathan Cousins <evolutionxbox@gmail.com>
2018-03-10 12:47:51 +0000

    Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets - Closes #10

--- src/UserInput/index.js
+++ src/UserInput/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import './UserInput.css';
 
-const UserInput = ({onChange, value}) => <input type="text" name="UserInput" onChange={onChange} value={value} />
+const UserInput = ({onChange, value}) => <input type="text" name="UserInput" className="UserInput" onChange={onChange} value={value} />
 
 export default UserInput;
--- src/UserOutput/index.js
+++ src/UserOutput/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+const styles = {
+  lineHeight: 1.2,
+  padding: '.5em',
+  margin: '2em',
+  borderRadius: '3px',
+  boxShadow: '0 0 3px rgba(0,0,0,.27)'
+}
+
 const UserOutput = ({username}) => (
-  <div className="UserOutput">
+  <div className="UserOutput" style={styles}>
     <p>Username: {username}</p>
     <p>Neque temporibus similique ratione placeat, commodi quibusdam quisquam, nisi. Ad iste esse vel quisquam quidem saepe dolores, dolorum est eaque vitae voluptate animi illo nisi. Molestiae quod pariatur odit aspernatur.</p>
     <p>Illo labore qui laboriosam sint sapiente, quidem repellendus delectus vitae doloremque, consequuntur praesentium, beatae vel! Cum minima dolore perferendis nulla pariatur quis expedita, quisquam sit et, ab nisi deleniti quam?</p>
