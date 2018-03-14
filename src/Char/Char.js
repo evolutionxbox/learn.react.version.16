abc35ef83f09b6b338bd9ebe2589a9875600ec9d
Jonathan Cousins <evolutionxbox@gmail.com>
2018-03-14 17:05:21 +0000

    Closes #14
    Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).

---
+++ src/Char/Char.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const styles = {
+  display: 'inline-block',
+  padding: '16px',
+  textAlign: 'center',
+  margin: '16px',
+  border: '1px solid black'
+}
+
+const Char = () => {
+  return <span style={styles} />
+}
+
+export default Char
